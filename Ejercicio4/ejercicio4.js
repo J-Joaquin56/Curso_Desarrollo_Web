daa541e8f4a634762228c6907b2064a0eab67db7
J-Joaquin56 <josejoaquin5631@gmail.com>
2024-05-19 23:27:12 -0600

    Se realizo Ejercicio 4

---
+++ Ejercicio4/ejercicio4.js
@@ -0,0 +1,12 @@
+// Declaración de variables
+let caracter = 'a';
+let vecesARepetir = 10;
+let resultado = '';
+
+// Lógica para repetir el carácter
+for (let i = 0; i < vecesARepetir; i++) {
+    resultado += caracter;
+}
+
+// Mostrar el resultado
+console.log(resultado);
